fe1ca55ba508a29ca9c6eaccebcb574c87a8f659
alex <hello@goose.im>
2014-03-28 14:04:33 -0400

    style

--- index.php
+++ index.php
@@ -6,23 +6,10 @@ include 'header.php';
 </style>
 
 <body>	
-<div id="taco">
-	<h2>this is <a href="http://mangouste.net/">goose</a>'s academic portfolio.</h2>
+<div id="alpha">
+	<h2>hello</h2>
 	<div class="links">
-		<?php echo $content['index_links.txt']; ?>
+		<?php echo $content['content.txt']; ?>
 	</div>
 </div>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-25040530-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 </body>
--- style.php
+++ style.php
@@ -2,41 +2,18 @@
 $s = parse_ini_file("./admin/settings.ini");
 ?>
 
-@font-face {
-  font-family: Socialico;
-  src: url("Socialico.otf");
-}
-* {padding:0;margin:0;outline:none;}
-body {font-weight: 100;background:#333;padding:0;margin:0;color:#eee;font-family:'Ubuntu',Helvetica Neue,Helvetica ,Arial,sans-serif;height:100%;overflow:auto;padding-bottom:80px;}
+@import url(http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300);
 
-h2 {font-size:30px;font-weight:500;width:100%;}
+* {padding:0;margin:0;outline:none;}
+body {font-family:'Open Sans',sans-serif;}
+
+h2 {font-size:30px;font-weight:300;width:100%;}
 
 h2 a {color:#eee;text-decoration:none;opacity:0.6;}
 
 ::selection {background:<?=$s[light_color];?>;}
 
-#taco {margin-top:7%;margin-left:45%;width:50%;}
-
-@-webkit-keyframes fingerbounce {
- from {
-   margin-bottom: 10px;
-   margin-top: 0px;
- }
- to {
-   margin-bottom: 20px;
-   margin-top: -10px;
- }
-}
-
-#finger {margin-left:257px;-webkit-animation:fingerbounce 1s infinite alternate ease-in-out;visibility: visible;opacity:1;}
-/*#finger:hover {margin-top:-10px;margin-bottom:10px;}*/
-
-
-.links a {width:66%;display:block;padding:15px;font-size:20px;background:#eee;margin:30px 20px 20px 5px;color: <?=$s[dark_color];?>;font-weight:500;border-radius:10px;border-top-left-radius:0px;border-right:20px solid #eee;}
-
-.links a:hover {opacity:0.8;}
-
-.external a {border-right:20px solid <?=$s[light_color];?>;}
+.alpha {margin-top:7%;margin-left:45%;width:50%;}
 
 ::-webkit-scrollbar              {width:9px;height:9px;background: <?=$s[dark_color];?>;}
 ::-webkit-scrollbar-button       {}
